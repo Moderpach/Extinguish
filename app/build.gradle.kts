af50abc060dc7ab3b2b3c65593b6a5b288aca7d5
Moderpach <34941512+Moderpach@users.noreply.github.com>
2024-09-04 10:52:54 +0800

    [fix] signingConfig in release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
     compileOptions {
