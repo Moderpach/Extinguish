2481d4db3f648c871afae000770a3b5717658d90
Moderpach <34941512+Moderpach@users.noreply.github.com>
2024-09-12 13:06:24 +0800

    bump 0.9.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "own.moderpach.extinguish"
         minSdk = 26
         targetSdk = 35
-        versionCode = 31
-        versionName = "0.9.2"
+        versionCode = 32
+        versionName = "0.9.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         vectorDrawables {
