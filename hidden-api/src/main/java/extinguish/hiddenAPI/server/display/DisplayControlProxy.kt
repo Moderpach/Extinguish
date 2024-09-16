37799004102a27ee14f88633bb8bdb5f3527400e
Moderpach <34941512+Moderpach@users.noreply.github.com>
2024-09-16 12:49:32 +0800

    [refactor] change hidden api proxy to object

--- hidden-api/src/main/java/extinguish/hiddenAPI/server/display/DisplayControlProxy.kt
+++ hidden-api/src/main/java/extinguish/hiddenAPI/server/display/DisplayControlProxy.kt
@@ -5,52 +5,50 @@ import android.os.IBinder
 import java.lang.reflect.Method
 
 @SuppressLint("BlockedPrivateApi", "PrivateApi")
-class DisplayControlProxy {
+object DisplayControlProxy {
 
-    companion object {
-        private val CLASS: Class<*> by lazy {
-            val classLoaderFactoryClass =
-                Class.forName("com.android.internal.os.ClassLoaderFactory")
-            val createClassLoaderMethod = classLoaderFactoryClass.getDeclaredMethod(
-                "createClassLoader",
-                String::class.java,
-                String::class.java,
-                String::class.java,
-                ClassLoader::class.java,
-                Int::class.javaPrimitiveType,
-                Boolean::class.javaPrimitiveType,
+    private val CLASS: Class<*> by lazy {
+        val classLoaderFactoryClass =
+            Class.forName("com.android.internal.os.ClassLoaderFactory")
+        val createClassLoaderMethod = classLoaderFactoryClass.getDeclaredMethod(
+            "createClassLoader",
+            String::class.java,
+            String::class.java,
+            String::class.java,
+            ClassLoader::class.java,
+            Int::class.javaPrimitiveType,
+            Boolean::class.javaPrimitiveType,
+            String::class.java
+        )
+        val classLoader = createClassLoaderMethod.invoke(
+            null, "/system/framework/services.jar", null, null,
+            ClassLoader.getSystemClassLoader(), 0, true, null
+        ) as ClassLoader
+
+        classLoader.loadClass("com.android.server.display.DisplayControl").also {
+            val loadMethod = Runtime::class.java.getDeclaredMethod(
+                "loadLibrary0",
+                Class::class.java,
                 String::class.java
             )
-            val classLoader = createClassLoaderMethod.invoke(
-                null, "/system/framework/services.jar", null, null,
-                ClassLoader.getSystemClassLoader(), 0, true, null
-            ) as ClassLoader
-
-            classLoader.loadClass("com.android.server.display.DisplayControl").also {
-                val loadMethod = Runtime::class.java.getDeclaredMethod(
-                    "loadLibrary0",
-                    Class::class.java,
-                    String::class.java
-                )
-                loadMethod.isAccessible = true
-                loadMethod.invoke(Runtime.getRuntime(), it, "android_servers")
-            }
+            loadMethod.isAccessible = true
+            loadMethod.invoke(Runtime.getRuntime(), it, "android_servers")
         }
+    }
 
-        private val METHOD_getPhysicalDisplayIdsMethod: Method by lazy {
-            CLASS.getMethod("getPhysicalDisplayIds")
-        }
+    private val METHOD_getPhysicalDisplayIdsMethod: Method by lazy {
+        CLASS.getMethod("getPhysicalDisplayIds")
+    }
 
-        private val METHOD_getPhysicalDisplayTokenMethod: Method by lazy {
-            CLASS.getMethod("getPhysicalDisplayToken", Long::class.javaPrimitiveType)
-        }
+    private val METHOD_getPhysicalDisplayTokenMethod: Method by lazy {
+        CLASS.getMethod("getPhysicalDisplayToken", Long::class.javaPrimitiveType)
+    }
 
-        fun getPhysicalDisplayIds(): LongArray {
-            return METHOD_getPhysicalDisplayIdsMethod.invoke(null) as LongArray
-        }
+    fun getPhysicalDisplayIds(): LongArray {
+        return METHOD_getPhysicalDisplayIdsMethod.invoke(null) as LongArray
+    }
 
-        fun getPhysicalDisplayToken(physicalDisplayId: Long): IBinder {
-            return METHOD_getPhysicalDisplayTokenMethod.invoke(null, physicalDisplayId) as IBinder
-        }
+    fun getPhysicalDisplayToken(physicalDisplayId: Long): IBinder {
+        return METHOD_getPhysicalDisplayTokenMethod.invoke(null, physicalDisplayId) as IBinder
     }
 }
