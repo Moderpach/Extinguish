7c7f5ea42b3d69091a86c2e7fb565a9513d82c6e
Moderpach <34941512+Moderpach@users.noreply.github.com>
2024-09-29 23:09:24 +0800

    [fix] check activity exist before start it

--- app/src/main/java/own/moderpach/extinguish/ui/navigation/ContextExt.kt
+++ app/src/main/java/own/moderpach/extinguish/ui/navigation/ContextExt.kt
@@ -2,13 +2,14 @@ package own.moderpach.extinguish.ui.navigation
 
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager.MATCH_DEFAULT_ONLY
 import android.net.Uri
 import android.widget.Toast
 import own.moderpach.extinguish.R
 
 fun Context.openUrlInBrowser(url: String) {
     val intent = Intent(Intent.ACTION_VIEW).setData(Uri.parse(url))
-    if (intent.resolveActivity(packageManager) != null) {
+    if (hasTargetActivity(intent)) {
         startActivity(intent)
     } else {
         val text = getString(R.string.str_browser_not_found)
@@ -21,10 +22,14 @@ fun Context.openShizuku() {
         "moe.shizuku.privileged.api",
         "moe.shizuku.manager.MainActivity"
     ).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-    if (intent.resolveActivity(packageManager) != null) {
+    if (hasTargetActivity(intent)) {
         startActivity(intent)
     } else {
         val text = getString(R.string.str_shizuku_not_found)
         Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
     }
+}
+
+fun Context.hasTargetActivity(intent: Intent): Boolean {
+    return packageManager.queryIntentActivities(intent, MATCH_DEFAULT_ONLY).size > 0
 }
